1c4f02b9aefa86f03ea7c3703252cd1328678b96
gomaja <marwanjdid@gmail.com>
2025-11-25 23:48:49 +0200

    feat: adds user structs for UpdateLocation

--- go-map.go
+++ go-map.go
@@ -35,3 +35,31 @@ type MoFsm struct {
 	MSISDN                 string
 	TPDU                   tpdu.TPDU // tpdu.MO (Submit type TPDU)
 }
+
+type UpdateLocation struct {
+	IMSI      string
+	MSCNumber string
+	VLRNumber string
+
+	VlrCapability *VlrCapability
+}
+
+type VlrCapability struct {
+	SupportedCamelPhases       *SupportedCamelPhases
+	SupportedLCSCapabilitySets *SupportedLCSCapabilitySets
+}
+
+type SupportedCamelPhases struct {
+	Phase1 bool
+	Phase2 bool
+	Phase3 bool
+	Phase4 bool
+}
+
+type SupportedLCSCapabilitySets struct {
+	LcsCapabilitySet1 bool
+	LcsCapabilitySet2 bool
+	LcsCapabilitySet3 bool
+	LcsCapabilitySet4 bool
+	LcsCapabilitySet5 bool
+}
