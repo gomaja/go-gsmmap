849df252953cd14ddf460f6812216481f74e6bbe
gomaja <marwanjdid@gmail.com>
2026-01-02 13:02:03 +0100

    ref: replace TODO comments

--- asn1mapmodel/map-dialogueinformation.go
+++ asn1mapmodel/map-dialogueinformation.go
@@ -1,4 +1,4 @@
 package asn1mapmodel
 
-// TODO: add map dialogue information
+// Add the map dialogue information
 // https://github.com/boundary/wireshark/blob/master/asn1/gsm_map/MAP-DialogueInformation.asn
